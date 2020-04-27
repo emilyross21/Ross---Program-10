d049abd7210c1dd68e78a4b728d71b6c9eece55c
Emily Ross <e.ross2@setonhill.edu>
2020-04-27 18:52:28 -0400

    Initial Commit

---
+++ Ross - Program 10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ross - Program 10
+//
+//  Created by Emily Ross on 4/27/20.
+//  Copyright © 2020 Emily Ross. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
